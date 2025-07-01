775f64f8e0d02e5010c91ff14a3f21f3ea04d589
Abhay Singh <78397691+ChaudharyAbhay@users.noreply.github.com>
2025-07-01 11:36:44 +0530

    Update next.config.ts

--- main/next.config.ts
+++ main/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+   basePath: '/main',
   typescript: {
     ignoreBuildErrors: true, // <-- THIS skips TS errors during build
   },
